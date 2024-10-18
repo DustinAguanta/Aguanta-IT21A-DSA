4a6eaa3fff6dfd1fc2f41a2ee9485e8f7a46f4a5
DustinAguanta <dustinagustin011@gmail.com>
2024-10-18 18:54:58 +0800

    Add files via upload

---
+++ Activity2.java
@@ -0,0 +1,32 @@
+
+
+package activity2;
+
+import java.util.Stack;
+
+public class Activity2 {
+
+        public static void main(String[] args) {
+        
+            Stack<String> mystack = new Stack<>();
+        
+        
+        mystack.push("3");
+        mystack.push("4");
+        mystack.push("7");
+         
+        System.out.println("Stack: " + mystack);
+        String topElement = mystack.pop();
+        System.out.println("Popped: " + topElement);
+       
+           String peekedElement = mystack.peek();
+        System.out.println("Peeked: " + peekedElement);
+        
+        boolean isEmpty = mystack.isEmpty();
+        System.out.println("Is Stack Empty? " + isEmpty);
+        
+         int stackSize = mystack.size();
+        System.out.println("Stack Size: " + stackSize);
+    }
+    
+}
